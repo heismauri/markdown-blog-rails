98a922a145b2e6545badb304a59a679238195147
Mauricio Villa <vbamauricio@gmail.com>
2023-01-18 22:26:49 -0300

    Upgraded app from Rails 6.1 to Rails 7

--- config/application.rb
+++ config/application.rb
@@ -8,13 +8,8 @@ Bundler.require(*Rails.groups)
 
 module MarkdownBlogRails
   class Application < Rails::Application
-    config.generators do |generate|
-      generate.assets false
-      generate.helper false
-      generate.test_framework :test_unit, fixture: false
-    end
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
